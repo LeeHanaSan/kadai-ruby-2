85b7e2f7ccf9d8ba2624181d4feb50b37fa406a6
Arisa Ichigaya <prog.ichigaya@gmail.com>
2022-03-03 09:42:32 +0000

    kadai-ruby-2

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,17 +1,17 @@
 def fizzbuzz(num)
   if num % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else
-    puts num
+    num
   end
 end
 
 max_number = 100
 
 (1..max_number).each do |nums|
-  fizzbuzz(nums)
+  puts fizzbuzz(nums)
 end
